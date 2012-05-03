3faf6ab9eb386a1a9853c1428355061fab418679
Adam Backstrom <adam@sixohthree.com>
2012-05-03 12:02:04 -0400

    Make binary.php work in new webapp structure

--- webapp/binary.php
+++ webapp/binary.php
@@ -1,99 +1,97 @@
 <?php 
+
+require dirname( __DIR__ ) . '/legacy/git-bootstrap.php';
+
+/*
+ * Stream a binary file if logged in. Requires rewrite rule for each filetype.  
+ */
+
 ob_end_clean();
-// version 1.0
-// last updated: 1/24/06
-// stream a binary file if logged in through portal CAS, requires rewrite rule for each filetype.  Rule currently applies to any subfolder named secure with SWF
-// Zach Tirrell
 
 require_once 'autoload.php';
 PSU::session_start();
 
-$WEB_ROOT = '/web/pscpages';
+/*
+ * Determine directories in which we'll look for this file.
+ */
+$path = $_SERVER['REQUEST_URI'];
 
-if(!file_exists($WEB_ROOT.$_SERVER['REDIRECT_URL'])) {
+$valid_roots = array(
+	'/web/pscpages', // legacy web root
+);
+
+$parts = explode( '/', $path );
+
+// app and webapp can be pulled from this repository
+if( in_array( $parts[1], array( 'app', 'webapp' ), true ) ) {
+	$valid_roots[] = dirname( __DIR__ );
+}
+
+// Iterate all valid document roots, looking for this file
+foreach( $valid_roots as $root ) {
+	if( $fullpath = realpath( $root . '/' . $path ) ) {
+		if( substr( $fullpath, 0, strlen( $root ) + 1 ) === $root . '/' ) {
+			break;
+		}
+	}
+
+	$fullpath = false;
+}
+
+// Production currently specifies "RewriteCond %{REQUEST_FILENAME} -f", so 
+// in theory we won't reach this line of code.
+if( false == $fullpath || ! is_file( $fullpath ) ) {
 	header('HTTP/1.1 404 Not Found');
-	exit('File not found error ('.$WEB_ROOT.$_SERVER['REDIRECT_URL'].')');
+	exit('File not found error (' . $path . ')');
 }
 
 $user = IDMObject::authN();
 
-$temp = preg_split('/secure\//i',$_SERVER['REDIRECT_URL']);
-$temp[count($temp)-1] = '.htrole';
-$htrole = $WEB_ROOT.implode('secure/',$temp);
+$fullpath_dir = dirname( $fullpath );
 
-if(file_exists($htrole)) {
-	$roles = file($htrole);
-
-	$pid = PSU::get('idmobject')->getIdentifier($user,'username','pid');
-	$user_roles = PSU::get('idmobject')->getAllBannerRoles($pid);
-	$ok = false;
-
-	foreach($roles as $role) {
-		$ok |= in_array(trim($role),$user_roles);
+// Find path to our "secure" directory
+if( '/secure' === substr( $fullpath_dir, -7 ) ) {
+	$secure_dir = $fullpath_dir;
+} else {
+	$parts = explode( '/', $fullpath_dir );
+	while( 'secure' !== array_pop( $parts ) ) {
+		if( 0 === count( $parts ) ) {
+			break;
+		}
 	}
 
-	if(!$ok) {
-		header('HTTP/1.1 403 Forbidden');
-		exit('You do not have access to the requested file.');
-	}
+	$secure_dir = implode( '/', $parts ) . '/secure';
 }
 
-$file = $WEB_ROOT.$_SERVER['REDIRECT_URL'];
-$type = 'octet-stream'; // default
-$pos = strrpos( $file, '.' );
+// Examine optional .htrole
+if( file_exists( $htrole = $secure_dir . '/.htrole' ) ) {
+	$roles = file( $htrole );
 
-// pull out the file suffix for Content-type
-if( $pos !== false ) {
-	$suffix = substr( $file, $pos + 1 );
-
-	// if suffix is just letters/numbers...
-	if( ctype_alnum($suffix) ) {
-		$type = $suffix;
+	foreach( $roles as $role ) {
+		if( ! in_array( trim($role), $_SESSION['AUTHZ']['banner'] ) ) {
+			header('HTTP/1.1 403 Forbidden');
+			exit('You do not have access to the requested file.');
+		}
 	}
 }
 
-$len = filesize($file);
-
-header('Pragma: ');
-header('Cache-Control: ');
-
-header('Content-type: application/'.$type);
-header("Content-Length: $len");
-//header('Content-Disposition: inline; filename=file1.'.$type);
-header("Content-Transfer-Encoding: binary\n");
-readfile_chunked($file);
-
-// zbtirrell@psu - problem with large file downloads resolved for 5.0.4, revisit after upgrade to 5.1+
-function readfile_chunked($filename,$retbytes=true) {
-   $chunksize = 1*(1024*1024); // how many bytes per chunk
-   $buffer = '';
-   $cnt = 0;
-   $handle = fopen($filename, 'rb');
-
-   if ($handle === false) {
-       return false;
-   }
-
-   while (!feof($handle)) {
-       $buffer = fread($handle, $chunksize);
-       echo $buffer;
-       if ($retbytes) {
-           $cnt += strlen($buffer);
-       }
-   }
-
-   $status = fclose($handle);
-   if ($retbytes && $status) {
-       return $cnt; // return num. bytes delivered like readfile() does.
-   }
-   return $status;
-
+switch( substr( $fullpath, strrpos( $fullpath, '.' ) + 1 ) ) {
+	case 'png':
+		$content_type = 'image/png'; break;
+	case 'jpg':
+	case 'jpeg':
+		$content_type = 'image/jpeg'; break;
+	case 'gif':
+		$content_type = 'image/gif'; break;
+	case 'pdf':
+		$content_type = 'application/pdf'; break;
+	default: $content_type = 'application/octet-stream';
 }
 
+header( 'Pragma: no-cache' );
+header( 'Cache-Control: no-cache, must-revalidate' );
+header( 'Content-Type: ' . $content_type );
+header( 'Content-Length: ' . filesize( $fullpath ) );
+header( 'Content-Transfer-Encoding: binary' );
 
-/* Required Rewrite:
-RewriteEngine On
-RewriteBase /
-RewriteCond   %{REQUEST_URI}  ^/(.*)secure/(.*).swf [NC]
-RewriteRule   ^(.*)    /webapp/binary.php  [L,E:TYPE=swf]
-*/
+readfile( $fullpath );
