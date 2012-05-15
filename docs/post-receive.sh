e6fd067b6af96b664286b07bd4f8182cb47c0181
Adam Backstrom <adam@sixohthree.com>
2012-05-15 09:23:58 -0400

    Add umask to post-receive script

--- docs/post-receive.sh
+++ docs/post-receive.sh
@@ -6,6 +6,8 @@
 
 GIT_WORK_TREE=/web/app/plymouth-webapp git checkout -f
 
+umask 0002
+
 while read oldrev newrev refname ; do
 	echo $(date "+%Y-%m-%dT%H:%M:%S") $oldrev $newrev $refname >>/web/app/logs/plymouth-webapp.pushlog
 done
