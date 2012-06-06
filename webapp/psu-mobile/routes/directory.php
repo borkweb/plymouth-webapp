533de61c5e2cc02565164ffedc7201e773b99659
Trevor Suarez <rican7@gmail.com>
2012-06-06 11:32:41 -0400

    Allowed for the directory search details to be accessed by a GET request too. :)

--- webapp/psu-mobile/routes/directory.php
+++ webapp/psu-mobile/routes/directory.php
@@ -27,13 +27,34 @@ respond( '/search/[:what]/?', function( $request, $response, $app ){
 });
 
 // Let's get the details of the person
+respond( 'GET', '/user/[:username]/?', function( $request, $response, $app ){
+	// Get the search parameter from the request (url encode it... it may contain spaces, etc)
+	$username = $request->param( 'username' );
+
+	// Let's get our results
+	$user_details = Mobile::directory_search( $username );
+
+	// If there was more than one result, someone didn't use this correctly
+	if ( count( $user_details ) > 1 ) {
+		// We'll let it work anyway, but let's warn the developer
+		$_SESSION['warnings'][] = 'More than 1 result returned. Username may have been too ambiguous.';
+	}
+
+	// Assign the user_details object to the template
+	$app->tpl->assign( 'user_data', $user_details[0] );
+});
+
+// Let's get the details of the person
 respond( 'POST', '/user/[:username]/?', function( $request, $response, $app ){
 	// Get the user detail data from the request. That way we can save an API call.
 	$user_details = json_decode( stripslashes( $request->param( 'user-details' ) ) );
 
 	// Assign the user_details object to the template
 	$app->tpl->assign( 'user_data', $user_details );
+});
 
+// Let's get the details of the person
+respond( '/user/[:username]/?', function( $request, $response, $app ){
 	// Display the template
 	$app->tpl->assign( 'show_page', 'directory-details' );
 	$app->tpl->display( '_wrapper.tpl' );
