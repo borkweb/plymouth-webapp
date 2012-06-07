aee956692fd09d141fea6c5e012e5bd25dd81643
Trevor Suarez <rican7@gmail.com>
2012-06-07 09:49:18 -0400

    Made the login/out routes have optional last slashes Also fixed the logout logic... but it still redirects. Hmmm.

--- webapp/psu-mobile/routes/login.php
+++ webapp/psu-mobile/routes/login.php
@@ -1,7 +1,7 @@
 <?php
 
 // Generic response (don't force the trailing slash: this should catch any accidental laziness)
-respond( '/', function( $request, $response, $app ) {
+respond( '/?', function( $request, $response, $app ) {
 	// Let's create a session variable, so we know where to redirect back to
 	$_SESSION['called_url'] = $request->param( 'redirect_to' );
 
@@ -14,7 +14,7 @@ respond( '/', function( $request, $response, $app ) {
 });
 
 // Let's make sure to redirect them to the originally called URL if they requested to
-respond( '/login_success/', function( $request, $response, $app ) {
+respond( '/login_success/?', function( $request, $response, $app ) {
 	if (!empty($_SESSION['called_url'])) {
 		// Redirect to the originally intended authentication url
 		header('Location: ' . $_SESSION['called_url']);
--- webapp/psu-mobile/routes/logout.php
+++ webapp/psu-mobile/routes/logout.php
@@ -1,18 +1,13 @@
 <?php
 
 // Generic response (don't force the trailing slash: this should catch any accidental laziness)
-respond( '/', function( $request, $response, $app ) {
+respond( '/?', function( $request, $response, $app ) {
 	// Let's create a session variable, so we know where to redirect back to
 	$_SESSION['called_url'] = $request->param( 'redirect_to' );
 
-	// De-Authenticate the user
-	//IDMObject::setupCAS();
-	//IDMObject::unauthN();
-
 	// Let's log the user out
-	session_start();
-	session_unset();	// Destroy all the session data
-	session_destroy();	// Destroy the session completely
+	IDMObject::setupCAS();
+	IDMObject::unauthN();
 
 	// If the "ajax" parameter was not sent along with the request
 	if ( ! $request->param( 'ajax' ) ) {
@@ -22,7 +17,7 @@ respond( '/', function( $request, $response, $app ) {
 });
 
 // Let's make sure to redirect them to the originally called URL if they requested to
-respond( '/logout_success/', function( $request, $response, $app ) {
+respond( '/logout_success/?', function( $request, $response, $app ) {
 	if ( ! empty( $_SESSION['called_url'] ) ) {
 		// Redirect to the originally intended authentication url
 		header('Location: ' . $_SESSION['called_url']);
