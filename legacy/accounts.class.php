b4deb685aa920bae93a61534705cf1f31bd8c498
Matthew Batchelder <borkweb@gmail.com>
2012-06-13 15:06:09 -0400

    Resolves #195. Only mark as student if not type X
    This fixes #195.  Rather than querying SGBSTDN directly, we should be
    querying the v_student_active view.

--- legacy/accounts.class.php
+++ legacy/accounts.class.php
@@ -19,7 +19,7 @@ require_once('BannerGeneral.class.php');
  * @module		accounts.class.php
  * @author		Matthew Batchelder <mtbatchelder@plymouth.edu>
  * @copyright 2007, Plymouth State University, ITS
- */ 
+ */
 class accounts
 {
 	var $banner;
@@ -45,9 +45,9 @@ class accounts
 		$this->datamart = $datamart;
 		$this->userdb = $userdb;
 		$this->banner_general = new BannerGeneral($banner);
-		
+
 		$this->idm = $idm;
-		
+
 		$this->debug = $debug;
 		if($web)
 		{
@@ -59,17 +59,17 @@ class accounts
 			$this->line = "\n";
 			$this->error = ' [ERROR]';
 		}//end else
-						
-		if($this->debug) 
+
+		if($this->debug)
 		{
 			echo 'Turning debug: ON'.$this->line;
 			$this->msg = ' (DEBUG) ';
 		}//end if
-		else 
+		else
 		{
 			echo 'Turning debug: OFF'.$this->line;
 			$this->msg = '';
-		}//end else		
+		}//end else
 	}//end constructor
 
 	/**
@@ -83,7 +83,7 @@ class accounts
 	{
 		return $this->userdb->Execute("DELETE FROM USER_TEMP WHERE STATUS_FLAG=1");
 	}//end clearTempTable
-	
+
 	/**
 	 * createTempRecord
 	 *
@@ -102,7 +102,7 @@ class accounts
 		} else {
 			$person->oz_username = $GLOBALS['PLUTO']->GetOne("SELECT user_uname FROM USER_DB WHERE pidm='".$person->pidm."' AND user_active!=0");
 		}//end else
-		
+
 		//if no username exists
 		if(!$person->oz_username)
 		{
@@ -128,7 +128,7 @@ class accounts
 					echo $this->line.':: [NOTICE] Skipping '.$error_helper.' ('.$person->pidm.').  No unique identifier (SSN, Foreign SSN, Cert Number, etc) provided.'.$this->line;
 			} else {
 				echo $this->line.':: New '.$error_helper.' User for OZ ('.$person->pidm.')'.$this->line;
-		
+
 				if(!$this->userdb->Execute($sql, $args))
 				{
 					echo $this->line.'::'.$this->error.' Unable to insert '.$error_helper.' record into USER_TEMP for ('.$person->pidm.')'.$this->line;
@@ -182,14 +182,14 @@ class accounts
 	function generatePopulations()
 	{
 		echo $this->line.":: Retrieving Employees and Non Employees from Banner".$this->line;
-		
+
 		//grab employees
-		$sql = "SELECT distinct pidm 
-		          FROM psu_identity.person_attribute 
-		         WHERE attribute in('staff', 'faculty', 'lecturer') 
+		$sql = "SELECT distinct pidm
+		          FROM psu_identity.person_attribute
+		         WHERE attribute in('staff', 'faculty', 'lecturer')
 		           AND type_id = 2
 		           AND not exists(SELECT 1 FROM spbpers WHERE spbpers_pidm = pidm AND spbpers_dead_ind = 'Y')";
-		
+
 		$this->emps = $this->idm->db->GetAll($sql);
 
 		//grab non employees
@@ -223,11 +223,11 @@ class accounts
 	function getSQL(&$person,$query)
 	{
 		$sql='';
-		
+
 		switch($query)
 		{
 			case 'insert_employee':
-				$sql = "INSERT INTO DATA_MART_BUILD 
+				$sql = "INSERT INTO DATA_MART_BUILD
 								(timeval,
 									pidm,
 									id,
@@ -255,16 +255,16 @@ class accounts
 									flag_emp,
 									confident,
 									ug_gr
-								) 
-								VALUES 
+								)
+								VALUES
 								(now(),
-								 ".$person->pidm.", 
-								 '".$person->id."', 
-								 '', 
+								 ".$person->pidm.",
+								 '".$person->id."',
+								 '',
 								 '".addslashes($person->first_name)."',
 								 '".addslashes(empty($person->middle_name) ? '' : substr($person->middle_name, 0, 1))."',
 								 '".addslashes($person->last_name)."',
-								 '".$person->username."', 
+								 '".$person->username."',
 								 '".$person->classification."',
 								 '".addslashes($person->title)."',
 								 '".addslashes($person->department)."',
@@ -286,7 +286,7 @@ class accounts
 								 '".$person->ug_gr."')";
 			break;
 			case 'insert_student':
-				$sql = "INSERT INTO DATA_MART_BUILD 
+				$sql = "INSERT INTO DATA_MART_BUILD
 								(timeval,
 								pidm,
 								id,
@@ -323,7 +323,7 @@ class accounts
 								confident,
 								ug_gr,
 								degree,
-								program) 
+								program)
 								VALUES (now(),
 								".$person->pidm.",
 								'".$person->id."',
@@ -391,7 +391,7 @@ class accounts
 									WHERE pidm=".$person->pidm;
 			break;
 			case 'update_student':
-				$sql="UPDATE DATA_MART_BUILD SET 	
+				$sql="UPDATE DATA_MART_BUILD SET
 								timeval=now(),
 								id='".$person->id."',
 								ssn='',
@@ -465,7 +465,7 @@ class accounts
 			{
 				echo '::'.$this->error.' Failed to truncate DATA_MART Table'.$this->line;
 			}//end else
-		}//end else		
+		}//end else
 	}//end migrateBuild
 
 	/**
@@ -479,11 +479,11 @@ class accounts
 	function testConnections()
 	{
 		echo $this->line.":: Testing Database connections with DATA_MART, BANNER, and PLUTO".$this->line;
-		
+
 		$banner_connect = $this->banner->IsConnected();
 		$datamart_connect = $this->datamart->IsConnected();
 		$userdb_connect = $this->userdb->IsConnected();
-		
+
 		if($banner_connect && $datamart_connect && $userdb_connect)
 		{
 			echo ":: Connections successful!".$this->line;
@@ -495,7 +495,7 @@ class accounts
 			if(!$banner_connect) $fail[] = 'BANNER';
 			if(!$datamart_connect) $fail[] = 'DATA_MART';
 			if(!$userdb_connect) $fail[] = 'USER_DB';
-			
+
 			echo "::".$this->error." Connections FAILED for: ".implode(', ',$fail).$this->line;
 			return false;
 		}//end else
@@ -512,7 +512,7 @@ class accounts
 	function truncateBuild()
 	{
 		echo $this->line.":: ".$this->msg."Truncating DATA_MART_BUILD Table".$this->line;
-		
+
 		if(!$this->debug)
 		{
 			return $GLOBALS['DATA_MART']->Execute("TRUNCATE TABLE DATA_MART_BUILD");
@@ -523,7 +523,7 @@ class accounts
 		}//end else
 		return false;
 	}//end truncateBuild
-	
+
 	/**
 	 * updateDatamart
 	 *
@@ -549,7 +549,7 @@ class accounts
 				{
 					$sql = $this->getSQL($person,'update_employee');
 				}//end else
-				
+
 				if($this->debug)
 				{
 					echo $this->line.':: (DEBUG) DATA_MART update for (PIDM: '.$person->pidm.'): '.$sql.$this->line;
@@ -619,10 +619,10 @@ class accounts
 				$person->_load_phone();
 
 				$classifications = $this->idm->getPersonAttributesByMeta($person->pidm, 'classification');
-				
+
 				$person->classification = $emeritus ? 'RM' : key($classifications['role']);
 				$person->_load_ssn('//');
-				
+
 				$person->attributes = $this->idm->getPersonAttributes($person->pidm);
 
 				$person->data['flags'] = array();
@@ -650,16 +650,16 @@ class accounts
 				$of_phone = @current($of_phone);
 
 				$of_building_room = explode('Rm', $of_address['r_street_line2']);
-				
+
 				$person->msc = addslashes(trim($ca_address['r_street_line1']));
 				$person->building = addslashes(trim($of_building_room[0]));
 				$person->room = addslashes(trim($of_building_room[1]));
-				
+
 				$person->of_phone = preg_replace('/^535/', '', $of_phone['r_phone_number']);
-				
+
 				$person->ug_gr = 'NA';
 				$person->confidential = 'NA';
-				
+
 				if($emeritus) $this->createTempRecord($person, 'ALUMNI');
 				else $this->createTempRecord($person,'EMP');
 				$this->updateDatamart($person);
@@ -674,7 +674,7 @@ class accounts
 				gc_collect_cycles();
 			}//end if
 		}//end foreach
-		
+
 		echo $this->line.":: Finished Updating $person_type".$this->line;
 	}//end updateEmployees
 
@@ -689,17 +689,17 @@ class accounts
 	function updateNonEmployees()
 	{
 		echo $this->line.":: Updating Non-Employees (count: ".$this->count['non_emps'].")".$this->line;
-		
+
 		$i = 0;
 		foreach($this->non_emps as $row) {
 			$person = new PSUPerson($row['pidm']);
 			if($person->pidm)
 			{
 				$classifications = $this->idm->getPersonAttributesByMeta($person->pidm, 'classification');
-				
+
 				$person->classification = key($classifications['role']);
 				$person->_load_ssn('//');
-				
+
 				$person->attributes = $this->idm->getPersonAttributes($person->pidm);
 				$person->data['flags'] = array();
 				$person->data['flags']['emp'] = (isset($person->attributes['permission']['employee_list'])) ? 1 : '';
@@ -712,9 +712,9 @@ class accounts
 				$person->data['flags']['supervisor'] = (isset($person->attributes['permission']['supervisor_list'])) ? 1 : '';
 				$person->data['flags']['chair'] = (isset($person->attributes['permission']['chair_list'])) ? 1 : '';
 				$person->data['flags']['dept_contact'] = (isset($person->attributes['permission']['dept_contact_list'])) ? 1 : '';
-		
+
 				$this->createTempRecord($person, 'NON-EMP');
-				
+
 				$this->updateDatamart($person,false); //false == don't update datamart
 			}//end if
 			unset($person);
@@ -726,7 +726,7 @@ class accounts
 				gc_collect_cycles();
 			}//end if
 		}//end foreach
-		
+
 		echo $this->line.":: Finished Updating Non-Employees".$this->line;
 	}//end updateNonEmployees
 
@@ -784,13 +784,16 @@ class accounts
 						gobtpac_external_user r_username,
 						degree,
 						program
-				 FROM ps_as_student_demographics,gobtpac 
-				WHERE pidm=gobtpac_pidm 
-					AND NOT EXISTS (SELECT 1 
-									 FROM sgbstdn 
-									WHERE sgbstdn_styp_code IN('X') 
-										AND sgbstdn_pidm=pidm 
-										AND sgbstdn_term_code_eff = (SELECT MAX(sgbstdn_term_code_eff) FROM sgbstdn WHERE sgbstdn_pidm = pidm))";
+				 FROM ps_as_student_demographics,gobtpac
+				WHERE pidm=gobtpac_pidm
+					AND EXISTS (
+					      SELECT 1
+		              FROM v_student_active sa
+		             WHERE sa.styp_code NOT IN('X')
+									 AND sa.pidm=gobtpac_pidm
+							)
+		";
+
 		if($results=$this->banner->Execute($sql))
 		{
 			while($row=$results->FetchRow())
@@ -804,14 +807,14 @@ class accounts
 				$person->username = $row['r_username'];
 				$person->info = array();
 				$person->info = $row;
-				
+
 				$person->data['flags']['student'] = 1;
-							
+
 				$this->updateDatamart($person,true,true);
 
 				unset($person);
 			}//end while
-			
+
 			echo $this->line.":: Finished Updating Students".$this->line;
 		}//end if
 	}//end updateStudents
@@ -841,17 +844,17 @@ class accounts
 		if(($this->count['emps']+$this->count['non_emps'])<900)
 		{
 			echo $this->line."::".$this->error." number of employee/non-employee records returned by Banner is fewer than expected (count: ".$this->count['emps'].")".$this->line;
-			
+
 			if($this->count['emps']==0)
 			{
 				echo '::'.$this->error.' Employee list is empty!'.$this->line;
 			}//end if
-			
+
 			if($this->count['non_emps']==0)
 			{
 				echo '::'.$this->error.' Non Employee list is empty!'.$this->line;
 			}//end if
-			
+
 			return false;
 		}//end if
 		elseif($this->count['students']<3000)
@@ -863,12 +866,12 @@ class accounts
 			return true;
 		}//end else
 	}//end verifyCounts
-	
+
 	function webFooter()
 	{
 		echo '</body></html>';
 	}//end webFooter
-	
+
 	function webHeader()
 	{
 		echo '<html><head><title>Account Datamart Update</title>
@@ -880,5 +883,3 @@ class accounts
 		<body>';
 	}//end webHeader
 }//end class accounts
-
-?>
