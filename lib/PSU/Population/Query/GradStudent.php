a0d7d0a19ad7d9c217512b194dbccb4d33b288d6
nrporter <nrporter1@gmail.com>
2012-08-22 09:24:02 -0400

    Modifying Grad Population to take custom id type.

--- lib/PSU/Population/Query/GradStudent.php
+++ lib/PSU/Population/Query/GradStudent.php
@@ -5,18 +5,27 @@
  */
 class PSU_Population_Query_GradStudent extends PSU_Population_Query {
 	public function query ( $args = array() ) {
-		$defaults = array();
+
+		$defaults = array(
+			'identifier' => 'sourced_id',
+		);
 
 		$args = PSU::params( $args, $defaults );
 
 		$sql = "
-			SELECT gobsrid_sourced_id FROM gobsrid, datamart.ps_as_student_demographics WHERE 
-			program NOT LIKE '%TRACK%' AND 
-			gobsrid_pidm=pidm AND
-			ug_gr='GR'
+			SELECT DISTINCT b.".$args['identifier']."
+			  FROM sgbstdn
+			  JOIN psu.v_student_account_active a
+				ON sgbstdn_pidm = a.pidm AND sgbstdn_term_code_eff = a.term_code_eff
+			  JOIN PSU_IDENTITY.person_identifiers b
+				ON a.pidm = b.pid
+			 WHERE sgbstdn_degc_code_1 <> '000000'
+			   AND sgbstdn_levl_code = 'GR'
 		";
 
-		$matches = PSU::db('banner')->GetCol( $sql );
+		\PSU::db('banner')->debug = true;
+		$matches = PSU::db('banner')->GetCol( $sql, $args );
+		
 		return $matches;
 	}
 }
