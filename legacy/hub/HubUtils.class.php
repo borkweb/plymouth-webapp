a51b710e1073d181f7f209420c4782fb3e79c81b
Betsy Coleman <bscoleman@plymouth.edu>
2012-10-02 11:17:31 -0400

    changes for adding new BM Report App

--- legacy/hub/HubUtils.class.php
+++ legacy/hub/HubUtils.class.php
@@ -17,12 +17,14 @@ class HubUtils
  */
 
 	public $hub_config_id_msgofday = 'message_of_the_day';
+	public $hub_config_id_monthlystaffmtgsched = 'monthly_staff_meeting_schedule';
 	public $hub_config_id_email_who = 'portal_email_names';
 	public $hub_config_id_email_addresses = 'portal_email_addresses';
 	public $hub_config_id_ir_admin = 'ir_notify_admin';
 	public $hub_config_id_ir_rec = 'ir_notify_rec';
 	public $hub_config_id_ar_admin = 'ar_notify_admin';
 	public $hub_config_id_ar_rec = 'ar_notify_rec';
+	public $hub_config_id_bmr_notify = 'bmr_notify';
 
 	public function __construct()
 	{
