3f0854bdc2fbc58b452b910053e62192d8bda504
Nathan Porter <nrporter1@gmail.com>
2013-01-09 10:34:08 -0500

    Fixing to grab psu_id's

--- lib/PSU/Population/Query/PaidEmployee.php
+++ lib/PSU/Population/Query/PaidEmployee.php
@@ -5,12 +5,14 @@
  */
 class PSU_Population_Query_PaidEmployee extends PSU_Population_Query {
 	public function query ( $args = array() ) {
-		$defaults = array();
+		$defaults = array(
+			'identifier' => 'sourced_id',
+		);
 
 		$args = PSU::params( $args, $defaults );
 
 		$sql = "
-			SELECT DISTINCT idnt.sourced_id 
+			SELECT DISTINCT idnt." . $args['identifier'] . " 
 			FROM psu_identity.person_identifiers idnt,
 				 psu.v_hr_psu_employee_active act 
 			WHERE idnt.pid = act.pidm
