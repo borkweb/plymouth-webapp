bc7fbd1fa20a29613126be979549d939ebefd990
Nathan Porter <nrporter1@gmail.com>
2013-02-20 08:02:47 -0500

    Modifying query for Roger

--- lib/PSU/Population/Query/GradStudent.php
+++ lib/PSU/Population/Query/GradStudent.php
@@ -14,13 +14,11 @@ class PSU_Population_Query_GradStudent extends PSU_Population_Query {
 
 		$sql = "
 			SELECT DISTINCT b.".$args['identifier']."
-			  FROM sgbstdn
-			  JOIN psu.v_student_account_active a
-				ON sgbstdn_pidm = a.pidm AND sgbstdn_term_code_eff = a.term_code_eff
+			  FROM PSU.v_student_account_active a
 			  JOIN PSU_IDENTITY.person_identifiers b
 				ON a.pidm = b.pid
-			 WHERE sgbstdn_degc_code_1 <> '000000'
-			   AND sgbstdn_levl_code = 'GR'
+			 WHERE degc_code <> '000000'
+			   AND levl_code = 'GR'
 		";
 
 		\PSU::db('banner')->debug = true;
